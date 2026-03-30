e487fcd1ca5c4f7acc170f2796f004b5d69320cc
oyetundesamueldamilare <oyetundesamueldamilare@gmail.com>
2026-03-30 15:34:10 +0100

    migration files after updating User Name

---
+++ TeamIndia.TalentFlow/TeamIndia.TalentFlow.Infrastructure/Migrations/20260329164824_ChangedAppUserNaming.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamIndia.TalentFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedAppUserNaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "FullName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "AspNetUsers",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
