8e91f8ee613698a726df7148f8c1d33f2ca341b1
Stanley Ikemefuna <stanley.ikemefuna@riantotechnologies.com>
2026-04-05 16:21:22 +0100

    Updated the I email services

--- TeamIndia.TalentFlow/TeamIndia.TalentFlow.Application/Interfaces/IEmailService.cs
+++ TeamIndia.TalentFlow/TeamIndia.TalentFlow.Application/Interfaces/IEmailService.cs
@@ -2,6 +2,6 @@ namespace TeamIndia.TalentFlow.Application.Interfaces;
 
 public interface IEmailService
 {
-    void SendConfirmationEmail(string email, string subject, string body);
+    Task SendConfirmationEmail(string email, string subject, string body);
     Task SendTemplateEmailAsync(string email, string subject, string templateName, IDictionary<string, string> placeholders);
 }
